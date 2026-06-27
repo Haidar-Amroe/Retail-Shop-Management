39c3e93e3639c3c95f08b3038886e3b496f827af
Haidar <haidaramroe71@gmail.com>
2026-06-27 20:12:48 +0700

    Adding InventoryData Modul

---
+++ RetailShopManagement/src/InventoryData.java
@@ -0,0 +1,43 @@
+public class InventoryData {
+
+    public static void loadInitialData(ModulInventory inventory) {
+        inventory.addItem("899001", "Mie Sedaap Korean Cheese Buldak", 80, 3500);
+        inventory.addItem("899002", "Sponge Crunch Strawberry", 45, 9500);
+        inventory.addItem("899003", "Biskuit Lexus Keju", 35, 8500);
+        inventory.addItem("899004", "Biskuit Better Sandwich", 100, 2500);
+        inventory.addItem("899005", "Mie Sedaap Goreng", 150, 3100);
+        inventory.addItem("899006", "Indomie Goreng Spesial", 120, 3500);
+        inventory.addItem("899007", "Chitato Sapi Panggang 68g", 60, 11500);
+
+        // --- KATEGORI: MINUMAN  ---
+        inventory.addItem("899008", "Susu Ultra Milk Cokelat 250ml", 90, 6500);
+        inventory.addItem("899009", "Cimory Squeeze Blueberry", 50, 10000);
+        inventory.addItem("899010", "Yakult Pack (Isi 5)", 40, 10500);
+        inventory.addItem("899011", "Le Minerale Air Mineral 600ml", 150, 3500);
+        inventory.addItem("899012", "Teh Pucuk Harum 350ml", 100, 4000);
+        inventory.addItem("899013", "Coca Cola Cans 330ml", 70, 7000);
+        inventory.addItem("899014", "Ichitan Thai Milk Tea", 55, 9000);
+
+        // --- KATEGORI: PERAWATAN TUBUH & MANDI ---
+        inventory.addItem("899015", "Sabun Cair Lifebuoy Refill", 50, 22500);
+        inventory.addItem("899016", "Shampoo Biore Anti Dandruff", 40, 28000);
+        inventory.addItem("899017", "Pasta Gigi Pepsodent 190g", 65, 13500);
+        inventory.addItem("899018", "Sabun Muka Garnier Men 100ml", 30, 32000);
+
+        // --- KATEGORI: KEBUTUHAN RUMAH TANGGA ---
+        inventory.addItem("899019", "Minyak Goreng Filma 2 Liter", 40, 36000);
+        inventory.addItem("899020", "Gula Pasir Gulaku 1kg", 50, 17500);
+        inventory.addItem("899021", "Deterjen Rinso Anti Noda 700g", 35, 19500);
+        inventory.addItem("899022", "Tisu Wajah Paseo 250 Sheets", 55, 14000);
+
+        // --- KATEGORI: CAMILAN & COKELAT ---
+        inventory.addItem("899023", "Silverqueen Almond 62g", 50, 16500);
+        inventory.addItem("899024", "Cadbury Dairy Milk 62g", 40, 15500);
+        inventory.addItem("899025", "Pocky Chocolate Double", 45, 9000);
+
+        // --- KATEGORI: ALAT TULIS KANTOR (ATK) ---
+        inventory.addItem("899026", "Buku Tulis Kiky A5 Pack", 30, 38000);
+        inventory.addItem("899027", "Pulpen Gel Kenko Black 0.5", 150, 3500);
+        inventory.addItem("899028", "Pensil 2B Faber Castell", 100, 4500);
+    }
+}
