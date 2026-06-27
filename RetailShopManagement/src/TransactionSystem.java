afe470c74ce5280c9bac45ed417c41618ea59a79
Haidar <haidaramroe71@gmail.com>
2026-06-27 11:16:20 +0700

    TransactionSystem Class

--- RetailShopManagement/src/TransactionSystem.java
+++ RetailShopManagement/src/TransactionSystem.java
@@ -1,9 +1,35 @@
 import java.util.Stack;
+import java.util.Map;
+import java.util.HashMap;
+
 
 public class TransactionSystem {
     private ModulInventory inventory;
+    private Map<String, CartItem> cart;
+    private Stack<TransactionAction> undo;
+
+    static class CartItem{
+        Product product;
+        int quantity;
+
+        public CartItem(Product product, int quantity){
+            this.product = product;
+            this.quantity = quantity;
+        }
+    }
+
+    static class TransactionAction {
+        String barcode; 
+        int quantity;
+
+        public TransactionAction(String barcode, int quantity){
+            this.barcode = barcode;
+            this.quantity = quantity;
+        }
+    }
 
     public TransactionSystem(ModulInventory inventory){
         this.inventory = inventory;
     }
+    
 }
