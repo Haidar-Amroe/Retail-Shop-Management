44bab75a9be50d7de1fc03d2496e6b65c7f11250
Haidar <haidaramroe71@gmail.com>
2026-06-27 10:35:36 +0700

    Transaction System 2

---
+++ RetailShopManagement/src/TransactionSystem.java
@@ -0,0 +1,9 @@
+import java.util.Stack;
+
+public class TransactionSystem {
+    private ModulInventory inventory;
+
+    public TransactionSystem(ModulInventory inventory){
+        this.inventory = inventory;
+    }
+}
